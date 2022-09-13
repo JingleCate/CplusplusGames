70f92bbfd4d2447f106b9fa60896e05c26905f8c
JingleCate <2155230480@qq.com>
2022-09-13 15:33:55 +0800

    Superiority

--- CoolRun/main.cpp
+++ CoolRun/main.cpp
@@ -26,11 +26,12 @@ IMAGE heros[12];					// 英雄实现奔跑
 int hDstX;							// 英雄x坐标
 int hDstY;							// 英雄Y坐标
 bool canJump;						// 英雄是否可以跳跃
-int degrees[10] = { 5,4,3,2,1,-1,-2,-3,-4,-5 };// hero上跳过程中的速度程度,符合实际物理规律
+int degrees[30];					// hero上跳过程中的速度程度,符合实际物理规律
 int dgIndex;						// degress数组索引
 bool update;
 
 IMAGE imgTortoise;					// 乌龟障碍物
+
 
 
 // 函数声明
@@ -39,6 +40,7 @@ void updateBackgroud();
 void bgSlide();
 void heroRun(int heroIndex);
 void keyCatch();
+void tortoiseProduct();
 
 
 
@@ -54,7 +56,8 @@ int main(void) {
 			keyCatch();
 			BeginBatchDraw();					// 缓冲打印
 			updateBackgroud();					// 背景渲染
-			heroRun(hIndex);			// 英雄渲染
+			heroRun(hIndex);					// 英雄渲染
+			tortoiseProduct();					// 乌龟障碍物渲染
 			EndBatchDraw();						// 结束
 			bgSlide();
 			if (canJump == true)				// hero跳跃过程中不奔跑
@@ -79,6 +82,14 @@ int main(void) {
 void init(int width = 1012, int height = 396) {
 	initgraph(width, height);
 
+	// 初始化degrees数组
+	int length = sizeof(degrees) / 4;
+	for (int i = 0; i < length; i++){
+		if (i < length / 2)
+			degrees[i] = length / 2 - i;
+		else
+			degrees[i] = length / 2 - i - 1;
+	}
 
 	// 加载背景图片
 	char name[64];
@@ -104,7 +115,7 @@ void init(int width = 1012, int height = 396) {
 	update = false;
 
 	// 加载乌龟图片
-	sprintf_s(name, "res/t%d.png", 1);
+	sprintf_s(name, "res/t1.png");
 	loadimage(&imgTortoise, name);
 	
 }
@@ -134,12 +145,12 @@ void heroRun(int heroIndex) {
 
 void jump() {
 
-	hDstY -= 8 * degrees[dgIndex];
+	hDstY -= 1 * degrees[dgIndex];
 
 	// 界定dgIndex范围防止溢出
 	dgIndex++;
-	if (dgIndex >= 10) {
-		dgIndex %= 10;
+	if (dgIndex >= 30) {
+		dgIndex %= 30;
 		canJump = true;
 	}
 }
@@ -166,3 +177,71 @@ void keyCatch() {
 }
 
 
+void tortoiseProduct(){
+	static int tDstY = 300;							// tortoise的y坐标
+	static int tSpeed = 6;							// tortoise移动速率
+	static int tDstX[2] = { WIDTH,WIDTH };			// 乌龟的x坐标, 允许两只
+	static bool isShow[2] = { false,false };		// 另一个乌龟是否显示
+	static int frameSum[2] = { 0,0 };				// 两只乌龟的出现时间
+	static bool begin = true;
+	static int sum = 0;
+	sum++;
+	/*putimagePNG2(tDstX, tDstY, &imgTortoise);
+	frameSum++;
+	tDstX -= tSpeed;
+	if (tDstX < imgTortoise.getwidth()) {
+		tDstX = WIDTH;
+	}
+
+	if (frameSum > 300 && (rand() % 10 >= 5)) {
+
+	}*/
+	
+
+	// 随机出现两只乌龟且保证乌龟之间的一定距离
+	//if (isShow[0] == false && isShow[1] == false) begin = true;
+	int rd = rand() % 10;
+	if ((sum >= 50 && isShow[0] != true && (rd >= 5)) || ((rd >= 5) && begin)) {
+		isShow[0] = true;
+		begin = false;
+	}
+		
+	rd = rand() % 10;
+	if ((sum >= 50 && isShow[1] != true && (rd >= 7) ) || ((rd >= 5) && begin)) {
+		isShow[1] = true;
+		begin = false;
+	}
+		
+	if (sum >= 50) sum %= 50;
+
+	// 到达边界后自动重新计数
+	if (tDstX[0] <= -imgTortoise.getwidth()) {
+		tDstX[0] = WIDTH;
+		isShow[0] = false;
+		frameSum[0] = 0;
+	}
+
+	if (tDstX[1] <= -imgTortoise.getwidth()) {
+		tDstX[1] = WIDTH;
+		isShow[1] = false;
+		frameSum[1] = 0;
+	}
+
+
+	if (isShow[0] == true) {
+		putimagePNG2(tDstX[0], tDstY, WIDTH, &imgTortoise);
+		tDstX[0] -= tSpeed;
+		frameSum[0]++;
+	}
+	if (isShow[1] == true) {
+		putimagePNG2(tDstX[1], tDstY, WIDTH, &imgTortoise);
+		tDstX[1] -= tSpeed;
+		frameSum[1]++;
+	}
+
+
+	
+
+
+
+}
